7fafe6d63db7cb06421cd782992b94badf057272
Bram Mittendorff <bram@pescheck.nl>
2024-07-17 18:07:00 +0200

    Added more threadsafe loading of openssl

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <QDebug>
+#include <openssl/crypto.h>
 
 int main(int argc, char *argv[])
 {
+    OPENSSL_init_crypto(OPENSSL_INIT_ADD_ALL_CIPHERS | OPENSSL_INIT_ADD_ALL_DIGESTS | OPENSSL_INIT_LOAD_CRYPTO_STRINGS, NULL);
     QApplication a(argc, argv);
 
     // Create an instance of MainWindow
